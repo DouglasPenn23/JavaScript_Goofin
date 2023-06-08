33bb59af2f23f0768c7317979ab9e9ea85bc49f5
DouglasPenn23 <Douglaspennington2013@gmail.com>
2023-06-08 08:59:06 -0400

    Javascript Interactivity Challenge

--- script.js
+++ script.js
@@ -16,7 +16,11 @@
         });
     }
 
+    document.getElementById("myBtn").addEventListener("click", displayDate);
 
+    function displayDate() {
+      document.getElementById("demo").innerHTML = Date();
+    }
     
 
 
